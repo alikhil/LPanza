26163e427b282abdb3c88f5a53646786d4503ec9
alikhil <alikhil@mail.ru>
2015-06-09 01:27:59 +0500

    Добавлена фунция userRegister. Добавляет нового юзера в список и выделяет ему танк

--- lpanza.js
+++ lpanza.js
@@ -2,10 +2,24 @@ var io;
 var gameSocket;
 
 var userIdNames = { };
-
+/*
+	constants
+*/
 var tanksHP = 10;
 var damagePerShot = 1;
 
+var mapWidth = 3000;
+var mapHeight = 3000;
+
+var distanceFromWall = 30;
+
+var tankWidth = 40;
+var tankLenght = 40;
+var tankTurretRadius = 12;
+var tankGunWidth = 4;
+var tankGunLenght = 25;
+
+var tanks = [ { } ];
 exports.initGame = function(sio, socket){
     io = sio;
     gameSocket = socket;
@@ -32,5 +46,52 @@ exports.initGame = function(sio, socket){
 
 function userRegister(user) {
 	var sock = this;
-	userIdNames[sock.id.toString().substr(0,5)] = user.user;
+	var userId = getUserId(sock.id);
+	userIdNames[userId] = user.user;
+	//Инициализация танка
+	var label = {};
+	label.hp = tankHP;
+	label.userName = user.user;
+	
+	var gun = { };
+	gun.width = tankGunWidth;
+	gun.lenght = tankGunLenght;
+	gun.color = getRandomColor();
+	
+	var turret = { };
+	turret.rotation = tank.rotation;
+	turret.radius = tankTurretRadius;
+	turret.gun = gun;
+	
+	var tank = { };
+	tank.position = getRandomPosition();
+	tank.rotation = getRandom(0,360);
+	tank.color = getRandomColor();
+	tank.width = tankWidth;
+	tank.lenght = tankLenght;
+	
+	tank.turret = turret;
+	tank.label = label;
+	
+	tanks[userId] = tank;
+}
+/*
+	Пока просто рандом, потом будем выбирать по менее заселенной местности
+*/
+function getRandomPosition(){
+	var pos = { };
+	pos.x = getRandom(distanceFromWall,mapWidth - distanceFromWall);
+	pos.y = getRandom(distanceFromWall,mapHeight - distanceFromWall);
+	return pos;
+}
+
+function getRandom(min, max){
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomColor(){
+	return [ getRandom(0,255), getRandom(0,255), getRandom(0,255) ];
+}
+function getUserId(socketId){
+	return socketId.toString().substr(0,5);
 }
