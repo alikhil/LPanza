34cc5e94261b10393b06163a3ac2ad6d0d81a4bf
turuslan <turuslan.devbox@gmail.com>
2015-06-10 10:12:18 +0500

    Код прорисовки обьектов вынесен в game.paint.js. game.input заменен на game.control. game.control rotate не работает (mousemove не вызывается). Добавлены внутренние функции app.positionMapToScreen, app.positionScreenToMap, getAngle, angleRadToDeg.

--- public/game.js
+++ public/game.js
@@ -3,10 +3,10 @@ $(document).ready(function () {
 	var canvas = {
 		element: undefined,
 		context: undefined,
-		xSize: 0,
-		ySize: 0,
-		ySizeParentPercent: 90,
-		xSizeParentPercent: 90,
+		width: NaN,
+		height: NaN,
+		heightParentPercent: 90,
+		widthParentPercent: 90,
 		RGBToCSS: function (R, G, B) {
 			var letters = '0123456789ABCDEF'.split('');
 			var color = '#';
@@ -21,11 +21,11 @@ $(document).ready(function () {
 		init: function () {
 			canvas.element = $("#gameCanvas");
 			canvas.context = canvas.element[0].getContext("2d");
+			canvas.width = 1;
+			canvas.height = 1;
 			canvas.element
-				.width(1)
-				.height(1);
-			canvas.xSize = 1;
-			canvas.ySize = 1;
+				.width(canvas.width)
+				.height(canvas.height);
 			canvas.resize();
 			$(window).resize(function () {
 				canvas.resize();
@@ -34,34 +34,44 @@ $(document).ready(function () {
 		resize: function () {
 			var parent = canvas.element.parent()[0],
 				tempImageData = canvas.context.getImageData(0, 0, 
-					canvas.xSize, 
-					canvas.ySize);
-			canvas.xSize = parent.offsetWidth*canvas.xSizeParentPercent/100; 
-			canvas.ySize = parent.offsetHeight*canvas.ySizeParentPercent/100;
+					canvas.width,
+					canvas.height);
+			canvas.width = parent.offsetWidth*(
+				canvas.widthParentPercent/100
+			);
+			canvas.height = parent.offsetHeight*(
+				canvas.heightParentPercent/100
+			);
 			canvas.context.save();
-			canvas.element.width(canvas.xSize);
-			canvas.element.height(canvas.ySize);
+			canvas.element.width(canvas.width);
+			canvas.element.height(canvas.height);
 			canvas.context.restore();
 			canvas.context.putImageData(tempImageData, 0, 0);
-			canvas.element.css("margin-top", ((canvas.ySize/canvas.xSize)*(100-canvas.ySizeParentPercent)/2)+"%");
+			canvas.element.css("margin-top", ((canvas.height/canvas.width)*(100-canvas.heightParentPercent)/2)+"%");
+
+			app.game.screenUserPosition =
+				app.positionMapToScreen(app.game.mapUserPosition);
 		},
 		bindGameEvents: function () {
 			canvas.element
-				.on('mousemove', function (event) {
-					var offset = $(this).offset();
-					app.gameEventMouseMove(
-						event.pageX - offset.left,
-						event.pageY - offset.top
-						);
-				})
 				.on('mousedown', function () {
-					app.gameEventClick();
+					app.controls.gameEventClick();
+				})
+				.on('mousemove', function (event) {
+					var offset = canvas.element.offset();
+					app.controls.gameEventMouseMove({
+						x: event.pageX - offset.left,
+						y: event.pageY - offset.top
+					});
 				});
 			$(window).on('keydown', function (event) {
-					app.gameEventKeyDown(String.fromCharCode(event.which));
+					app.controls.gameEventKeyDown(String.fromCharCode(event.which));
 				})
 				.on('keyup', function (event) {
-					app.gameEventKeyUp(String.fromCharCode(event.which));
+					app.controls.gameEventKeyUp(String.fromCharCode(event.which));
+				})
+				.on('blur', function () {
+					app.controls.gameEventBlur();
 				});
 		},
 		unbindGameEvents: function () {
@@ -72,12 +82,123 @@ $(document).ready(function () {
 		defaultUserName: 'Anonymous',
 		game: {
 			paintRect: {
-				width: 0,
-				height: 0
+				width: NaN,
+				height: NaN
 			},
-			userPosition: {
-				x: 0,
-				y: 0
+			mapUserPosition: {
+				x: NaN,
+				y: NaN
+			},
+			screenUserPosition: {
+				x: NaN,
+				y: NaN
+			}
+		},
+		controls: {
+			keyAcceleration: {
+				W: {x: 0, y: -1},
+				A: {x: -1, y: 0},
+				S: {x: 0, y: +1},
+				D: {x: +1, y: 0}
+			},
+			keyState: {
+				W: false,
+				S: false,
+				A: false,
+				D: false
+			},
+			rotation: 360,
+			acceleration: {
+				power: 0,
+				rotation: 360
+			},
+			gameEventBlur: function () {
+				// force key up
+				for(var index in app.controls.keyState) {
+					app.controls.keyState[index] = false;
+				}
+			},
+			gameEventClick: function () {
+				app.controls.gameControlShot();
+			},
+			gameEventMouseMove: function (position) {
+				var mapPosition = app.positionScreenToMap(position),
+					newRotation = app.angleRadToDeg(
+						app.getAngle(
+							mapPosition.x - app.game.mapUserPosition.x,
+							mapPosition.y - app.game.mapUserPosition.y
+						)
+					);
+				if(app.controls.rotation != newRotation) {
+					app.controls.rotation = newRotation;
+					app.controls.gameControlRotate();
+				}
+			},
+			gameEventKeyDown: function (key) {
+				for(var index in app.controls.keyState) {
+					if(key == index) {
+						app.controls.keyState[index] = true;
+						break;
+					}
+				}
+				app.controls.gameKeyboardEvent();
+			},
+			gameEventKeyUp: function (key) {
+				for(var index in app.controls.keyState) {
+					if(key == index) {
+						app.controls.keyState[index] = false;
+						break;
+					}
+				}
+				app.controls.gameKeyboardEvent();
+			},
+			gameKeyboardEvent: function () {
+				var xAcceleration = 0,
+					yAcceleration = 0,
+					newRotation,
+					newPower;
+				for(var index in app.controls.keyState) {
+					if(app.controls.keyState[index]) {
+						xAcceleration += app.controls.keyAcceleration[index].x;
+						yAcceleration += app.controls.keyAcceleration[index].y;
+					}
+				}
+				newPower = Math.sqrt(xAcceleration*xAcceleration+yAcceleration*yAcceleration);
+				newPower = (newPower < 1? newPower : 1);
+				if(newPower != 0) {
+					newRotation = app.angleRadToDeg(
+						app.getAngle(
+							xAcceleration,
+							yAcceleration
+						)
+					);
+				} else {
+					newRotation = app.controls.acceleration.rotation;
+				}
+				if(app.controls.acceleration.power != newPower ||
+					app.controls.acceleration.rotation != newRotation) {
+					app.controls.acceleration.power = newPower;
+					app.controls.acceleration.rotation = newRotation;
+					app.controls.gameControlAccelerate();
+				}
+			},
+			gameControlAccelerate: function () {
+				socket.emit('game.control', {
+					type: "accelerate",
+					rotation: app.controls.acceleration.rotation,
+					power: app.controls.acceleration.power
+				});
+			},
+			gameControlRotate: function () {
+				socket.emit('game.control', {
+					type: "rotate",
+					rotation: app.controls.rotation
+				});
+			},
+			gameControlShot: function () {
+				socket.emit('game.control', {
+					type: "shot"
+				});
 			}
 		},
 		initUI: function () {
@@ -94,49 +215,48 @@ $(document).ready(function () {
 				return false;
 			});
 		},
-		gameEventClick: function () {
-			socket.emit('game.input', {
-				type: "mouse.click"
-			});
-		},
-		gameEventMouseMove: function (x, y) {
-			socket.emit('game.input', {
-				type: "mouse.move",
-				rotation: app.gameGetMapRotation(
-					x - app.game.userPosition.x,
-					y - app.game.userPosition.y
+		positionMapToScreen: function (position) {
+			return {
+				x: app.game.paintRect.width*(
+					position.x/
+					canvas.width
+				),
+				y: app.game.paintRect.height*(
+					position.y/
+					canvas.height
 				)
-			});
+			};
 		},
-		gameGetMapRotation: function (xDelta, yDelta) {
-			var xMapDelta = app.game.paintRect.width*xDelta / canvas.xSize,
-				yMapDelta = app.game.paintRect.height*yDelta / canvas.ySize,
-				radians = Math.atan(yMapDelta / xMapDelta),
-				degrees;
-			if(xMapDelta < 0) {
-				radians += Math.PI;
+		positionScreenToMap: function (position) {
+			return {
+				x: canvas.width*(
+					position.x/
+					app.game.paintRect.width
+				),
+				y: canvas.height*(
+					position.y/
+					app.game.paintRect.height
+				)
+			};
+		},
+		getAngle: function (xDelta, yDelta) {
+			var angle = Math.atan(yDelta / xDelta);
+			if(xDelta < 0) {
+				angle += Math.PI;
 			}
-			degrees = 180*(radians/Math.PI);
-			return degrees;
+			if(angle < 0) {
+				angle += 2*Math.PI;
+			}
+			return angle;
 		},
-		gameEventKeyDown: function (key) {
-			socket.emit('game.input', {
-				type: "keyboard.down",
-				key: key
-			});
-		},
-		gameEventKeyUp: function (key) {
-			socket.emit('game.input', {
-				type: "keyboard.up",
-				key: key
-			});
+		angleRadToDeg: function (angle) {
+			return 180*(angle/Math.PI);
 		},
 		bindSocketEvents: function () {
 			socket.on('game.join.ok', function (packet) {
-				app.game.paintRect.width = packet.paintRect.width;
-				app.game.paintRect.height = packet.paintRect.height;
-				app.game.userPosition.x = app.game.paintRect.width/2;
-				app.game.userPosition.y = app.game.paintRect.height/2;
+				app.game.paintRect = packet.paintRect;
+				app.game.mapUserPosition.x = app.game.paintRect.width/2;
+				app.game.mapUserPosition.y = app.game.paintRect.height/2;
 				app.hideMenuView();
 				canvas.bindGameEvents();
 			});
@@ -172,4 +292,6 @@ $(document).ready(function () {
 	};
 	app.init();
 	canvas.init();
+	gamePaint.app = app;
+	gamePaint.canvas = canvas;
 });
---
+++ public/game.paint.js
@@ -0,0 +1,15 @@
+var gamePaint = {
+	app: undefined,
+	canvas: undefined,
+	paint: function (packet) {
+		// handle game.repaint message
+	},
+	drawTank: function (tank) {
+	},
+	drawTurret: function (turret) {
+	},
+	drawGun: function (tank) {
+	},
+	drawBullet: function () {
+	}
+};
