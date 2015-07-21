57eb60a554a9ea93f69bb62b492700fb109d19ec
alikhil <alikhil@mail.ru>
2015-07-21 21:22:27 +0500

    Revert "Изменены размеры танка в constants.js"
    Откатил размеры танков
    This reverts commit 4b70097715a3e0530e596ee68ba5c39192f63409.

--- constants.js
+++ constants.js
@@ -32,8 +32,8 @@ exports.distanceFromWall = 30;
 
 
 /*Размеры танка*/
-exports.tankWidth = 67;
-exports.tankLength = 96;
+exports.tankWidth = 50;
+exports.tankLength = 50;
 /*Радиус башни[устарело]*/
 exports.tankTurretRadius = 9;
 /*Параметры пушки[устарели-теперь пушка часть башни]*/
